21f2eb000588bd5fa6a466d215480e0d5962e660
Sanghrakshit12 <gautamsanghrakshit@gmail.com>
2024-07-11 22:46:06 +0530

    Update

--- apps/Merchant_App/app/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Metchant_App(){
-  return <>
-  <h1>Hello From Merchant</h1>
-  </>
-}
--- apps/User_App/app/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Metchant_App(){
-  return <>
-  <h1 className="bg-red-300">Hello From User</h1>
-  </>
-}
